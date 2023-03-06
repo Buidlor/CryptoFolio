71d2f8fc54008cb7839507dc5471aa2c0dffaee2
Buidlor <89695544+Buidlor@users.noreply.github.com>
2023-03-06 21:31:55 +0100

    imports for web3

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,30 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Portfolio from './pages/Portfolio'
 import Landing from './pages/Landing'
+import { createClient, configureChains, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import { arbitrum } from "wagmi/chains";
 
+const { provider, webSocketProvider } = configureChains([arbitrum], [publicProvider(), ]);
+
+const client = createClient({
+  provider,
+  webSocketProvider,
+  autoConnect: true,
+})
 
 function App() {
   return (
     <div className="content">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Landing/>} />
-          <Route path="/portfolio" element={<Portfolio />} />
-
-        </Routes>
-      </Router>
+      <WagmiConfig client={client}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Landing/>} />
+            <Route path="/portfolio" element={<Portfolio />} />
+          </Routes>
+        </Router>
+      </WagmiConfig>
     </div>
-   
   )
 }
 
