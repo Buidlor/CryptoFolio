578ab17ba8dc57d26f5ee503ccc3e943df4fd608
Buidlor <89695544+Buidlor@users.noreply.github.com>
2023-03-07 23:08:53 +0100

    have to start wallet connect on landing page, possible ?

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,6 +1,8 @@
 import Header from "../components/Header";
 import LandingContent from "../components/LandingContent";
 
+// https://docs.moralis.io/authentication-api/how-to-authenticate-users-with-metamask-using-react
+
 const Landing = () => {
     return (
         <div className="landing">
